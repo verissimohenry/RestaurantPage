e7611b17729b9bf9fd76a1450aa7e304bcad244e
verissimohenry <verissimohenry04@gmail.com>
2021-07-12 20:49:49 +0100

    modify app

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: 'src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
@@ -12,27 +12,22 @@ module.exports = {
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
-    rules: [{
-      test: /\\.(js|jsx)$/,
-      loader: 'babel-loader',
-    },
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/,
-      type: 'asset',
-    },
+    rules: [
+      {
+        test: /\\.(js|jsx)$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/,
+        type: 'asset',
+      },
 
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],
-    resolve: {
-      extensions: ['', '.js', '.jsx', '.css'],
-      modulesDirectories: [
-        'node_modules',
-      ],
-    },
   },
 };

db0d2330fe5e70c498f00e9694568d9fc0f8c4d2
verissimohenry <verissimohenry04@gmail.com>
2021-07-12 20:29:54 +0100

    modify app

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,37 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [{
+      test: /\\.(js|jsx)$/,
+      loader: 'babel-loader',
     },
-    plugins: [
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
-    module: {
-        rules: [
-            {
-                test: /\\.(js|jsx)$/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader','css-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/,
-                type: 'asset',
-            },
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/,
+      type: 'asset',
+    },
 
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.css'],
+    modulesDirectories: [
+      'node_modules',
+    ],
+  },
 };

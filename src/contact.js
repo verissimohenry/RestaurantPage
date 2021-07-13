ed89adc0f398cd0aaba3d47bdfb3e1c56f7ca345
verissimohenry <verissimohenry04@gmail.com>
2021-07-13 15:35:20 +0100

    modify menu.js

--- src/contact.js
+++ src/contact.js
@@ -2,10 +2,10 @@ const contact = `   <div class="home-con">
 <h1>CONTACT</h1><br>
 <h5>Phone: 123 456 789</h5><br>
 
-<h5>Addres: First of November 42, Sofia, BG</h5>
+<h5>Address: Adekule akala Ajao Estate 42, Lagos, NG</h5>
 <br><br>
 <h6>Open from:</h6><br>
 <h6>Monday till Friday</h6><br>
-<h6>From 09:00 till 18:00</h6>
+<h6>From 08:00 till 20:00</h6>
 </div>`;
 module.exports = contact;
--- src/menu.js
+++ src/menu.js
@@ -2,9 +2,9 @@ const menu = ` <div class="row m-5 p-5">
 <div class="col-4">
 
     <div class="card mb-3" style="width: 18rem;">
-        <img class="card-img-top" src="https://www.glutenfreepalate.com/wp-content/uploads/2018/08/Gluten-Free-Pizza-3.2-480x360.jpg" alt="Card image cap">
+        <img class="card-img-top" src="https://www.glutenfreepalate.com/wp-content/uploads/2016/03/Vanilla-Birthday-Cake-Donuts-4.2.jpg" alt="Card image cap">
         <div class="card-body">
-          <p class="card-text">Margarita.</p>
+          <p class="card-text">Vanilla Cake Donuts.</p>
           <p class="card-text"> 12$</p>
         </div>
     </div>

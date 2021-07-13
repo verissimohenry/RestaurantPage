ac2bc18cedc744a79cfb7ccc3d0fdc5890af0578
verissimohenry <verissimohenry04@gmail.com>
2021-07-13 16:23:13 +0100

    modify menu filr

--- src/menu.js
+++ src/menu.js
@@ -12,20 +12,20 @@ const menu = ` <div class="row m-5 p-5">
 <div class="col-4">
 
     <div class="card mb-3" style="width: 18rem;">
-        <img class="card-img-top" src="https://www.glutenfreepalate.com/wp-content/uploads/2018/08/Gluten-Free-Pizza-3.2-480x360.jpg" alt="Card image cap">
+        <img class="card-img-top" src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg" alt="Card image cap">
         <div class="card-body">
-          <p class="card-text">Margarita.</p>
-          <p class="card-text"> 12$</p>
+          <p class="card-text">salmon-fish-seafood-veggies-salad.</p>
+          <p class="card-text"> 20$</p>
         </div>
     </div>
 </div>
 <div class="col-4">
 
     <div class="card mb-3" style="width: 18rem;">
-        <img class="card-img-top" src="https://www.glutenfreepalate.com/wp-content/uploads/2018/08/Gluten-Free-Pizza-3.2-480x360.jpg" alt="Card image cap">
+        <img class="card-img-top" src="https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_960_720.jpg" alt="Card image cap">
         <div class="card-body">
-          <p class="card-text">Margarita.</p>
-          <p class="card-text"> 12$</p>
+          <p class="card-text">asparagus-steak-veal-steak-veal.</p>
+          <p class="card-text"> 15$</p>
         </div>
     </div>
 </div>

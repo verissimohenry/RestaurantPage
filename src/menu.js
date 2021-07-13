db3f27ee78682d29ce844efeed3a6db35dbf5553
verissimohenry <verissimohenry04@gmail.com>
2021-07-13 16:29:22 +0100

    modify main and menu .js file

--- src/menu.js
+++ src/menu.js
@@ -32,10 +32,10 @@ const menu = ` <div class="row m-5 p-5">
 <div class="col-4">
 
     <div class="card mb-3" style="width: 18rem;">
-        <img class="card-img-top" src="https://www.glutenfreepalate.com/wp-content/uploads/2018/08/Gluten-Free-Pizza-3.2-480x360.jpg" alt="Card image cap">
+        <img class="card-img-top" src="https://cdn.pixabay.com/photo/2015/03/26/09/39/cupcakes-690040_960_720.jpg" alt="Card image cap">
         <div class="card-body">
-          <p class="card-text">Margarita.</p>
-          <p class="card-text"> 12$</p>
+          <p class="card-text">cupcakes-.</p>
+          <p class="card-text"> 10$</p>
         </div>
     </div>
 </div>

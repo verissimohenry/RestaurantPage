6b87e1ddfa3e7a9726c962e535fa6f8f5bef6eb4
verissimohenry <verissimohenry04@gmail.com>
2021-07-14 14:17:49 +0100

    modify app

--- src/index.js
+++ src/index.js
@@ -38,34 +38,9 @@ function navBar() {
   content.appendChild(contentChild);
 }
 navBar();
-const home = document.getElementById('home');
-const contact = document.getElementById('contact');
-const menu = document.getElementById('menu');
 const homeContent = document.createElement('div');
 const menuContent = document.createElement('div');
 const contactContent = document.createElement('div');
-function homePage() {
-  if (contentChild.childNodes[0]) {
-    contentChild.removeChild(contentChild.childNodes[0]);
-  }
-  contentChild.appendChild(homeContent);
-
-  homeContent.innerHTML = homeInner;
-}
-function menuPage() {
-  if (contentChild.childNodes[0]) {
-    contentChild.removeChild(contentChild.childNodes[0]);
-  }
-  contentChild.appendChild(menuContent);
-  menuContent.innerHTML = menuInner;
-}
-function contactPage() {
-  if (contentChild.childNodes[0]) {
-    contentChild.removeChild(contentChild.childNodes[0]);
-  }
-  contentChild.appendChild(contactContent);
-  contactContent.innerHTML = contactInner;
-}
 
 homePage();
 home.addEventListener('click', homePage);

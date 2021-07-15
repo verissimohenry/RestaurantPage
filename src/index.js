71aa4097f6bb32f82b77327e614ec7a4d5fe84e6
verissimohenry <verissimohenry04@gmail.com>
2021-07-15 12:30:43 +0100

    fix linter error

--- src/index.js
+++ src/index.js
@@ -1,49 +1,73 @@
 import '../style/style.css';
 
-import {
-  home
-} from './components/home';
-import {
-  menu
-} from './components/menu';
-import {
-  contact
-} from './components/contact';
+import home from './home';
+import menu from './menu';
+import contact from './contact';
 
-const linkHome = document.querySelector('.link-home');
-const linkMenu = document.querySelector('.link-menu');
-const linkContact = document.querySelector('.link-contact');
-const homeDiv = document.querySelector('.home-div');
-const menuDiv = document.querySelector('.menu-div');
-const contactDiv = document.querySelector('.contact-div');
-// Show only home page
-linkHome.addEventListener('click', () => {
-  setDisplayBlock(homeDiv)
-  setDisplayNone(menuDiv)
-  setDisplayNone(contactDiv)
-});
+const content = document.getElementById('content');
+const contentChild = document.createElement('div');
+function navBar() {
+  const nav = document.createElement('nav');
+  const ul = document.createElement('ul');
+  const li1 = document.createElement('li');
+  const li2 = document.createElement('li');
+  const li3 = document.createElement('li');
+  const a1 = document.createElement('a');
+  const a2 = document.createElement('a');
+  const a3 = document.createElement('a');
+  li1.classList.add('list-item');
+  a1.innerHTML = 'Home';
+  a1.id = 'home';
+  li2.classList.add('list-item');
+  a2.innerHTML = 'Menu';
+  a2.id = 'menu';
+  li3.classList.add('list-item');
+  a3.innerHTML = 'Contact';
+  a3.id = 'contact';
+  a1.href = '#';
+  a2.href = '#';
+  a3.href = '#';
+  li1.appendChild(a1);
+  li2.appendChild(a2);
+  li3.appendChild(a3);
+  ul.appendChild(li1);
+  ul.appendChild(li2);
+  ul.appendChild(li3);
+  nav.appendChild(ul);
+  content.appendChild(nav);
+  content.appendChild(contentChild);
+}
+navBar();
+const home = document.getElementById('home');
+const contact = document.getElementById('contact');
+const menu = document.getElementById('menu');
+const homeContent = document.createElement('div');
+const menuContent = document.createElement('div');
+const contactContent = document.createElement('div');
+() => {
+  if (contentChild.childNodes[0]) {
+    contentChild.removeChild(contentChild.childNodes[0]);
+  }
+  contentChild.appendChild(homeContent);
 
-// Show only menu page
-linkMenu.addEventListener('click', () => {
-  setDisplayNone(homeDiv)
-  setDisplayBlock(menuDiv)
-  setDisplayNone(contactDiv)
-});
-
-// Show only contact page
-linkContact.addEventListener('click', () => {
-  setDisplayNone(homeDiv)
-  setDisplayNone(menuDiv)
-  setDisplayBlock(contactDiv)
-
-});
-
-// Sets display - none for passed element
-const setDisplayNone = (el) => {
-  el.style.display = 'none'
+  homeContent.innerHTML = homeInner;
+}
+() => {
+  if (contentChild.childNodes[0]) {
+    contentChild.removeChild(contentChild.childNodes[0]);
+  }
+  contentChild.appendChild(menuContent);
+  menuContent.innerHTML = menuInner;
+}
+() => {
+  if (contentChild.childNodes[0]) {
+    contentChild.removeChild(contentChild.childNodes[0]);
+  }
+  contentChild.appendChild(contactContent);
+  contactContent.innerHTML = contactInner;
 }
 
-// Sets display - block for passed element
-const setDisplayBlock = (el) => {
-  el.style.display = 'block'
-}
+homePage();
+home.addEventListener('click', homePage);
+contact.addEventListener('click', contactPage);
+menu.addEventListener('click', menuPage);

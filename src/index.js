5dad28d6a44b93f373a3a05c0fd2ad51580e3135
verissimohenry <verissimohenry04@gmail.com>
2021-07-19 15:19:05 +0100

    fix linter

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import home from './home';
 import menu from './menu';
 import contact from './contact';
 
-
 const homeInner = require('./home');
 const menuInner = require('./menu');
 const contactInner = require('./contact');
 const content = document.getElementById('content');
 const contentChild = document.createElement('div');
+
 function navBar() {
   const nav = document.createElement('nav');
   const ul = document.createElement('ul');

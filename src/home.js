e11f29095c47e484296457abd3e8303318834416
verissimohenry <verissimohenry04@gmail.com>
2021-07-19 17:32:00 +0100

    modif project based on tse review

--- src/home.js
+++ src/home.js
@@ -1,4 +1,6 @@
-const home = `   <div class="home-con">
+const home = () => {
+    const homeSection = document.createElement('div');
+    `   <div class="home-con">
 <h5>Best Restaurant in your country</h5><br>
 
 <h5>Made with love since 1999</h5>
@@ -6,4 +8,7 @@ const home = `   <div class="home-con">
 </div>
 <h4>Order online or visit us!</h4>
 </div>`;
+    return homeSection;
+};
+
 export default home;
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,7 @@
-const menu = ` <div class="row m-5 p-5">
-<div class="col-4">
+const menu = () => {
+  const menuDiv = document.createElement('div');
+  ` <div class="row m-5 p-5">
+ <div class="col-4">
 
     <div class="card mb-3" style="width: 18rem;">
         <img class="card-img-top" src="https://www.glutenfreepalate.com/wp-content/uploads/2016/03/Vanilla-Birthday-Cake-Donuts-4.2.jpg" alt="Card image cap">
@@ -8,8 +10,8 @@ const menu = ` <div class="row m-5 p-5">
           <p class="card-text"> 12$</p>
         </div>
     </div>
-</div>
-<div class="col-4">
+ </div>
+ <div class="col-4">
 
     <div class="card mb-3" style="width: 18rem;">
         <img class="card-img-top" src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg" alt="Card image cap">
@@ -18,8 +20,8 @@ const menu = ` <div class="row m-5 p-5">
           <p class="card-text"> 20$</p>
         </div>
     </div>
-</div>
-<div class="col-4">
+ </div>
+ <div class="col-4">
 
     <div class="card mb-3" style="width: 18rem;">
         <img class="card-img-top" src="https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_960_720.jpg" alt="Card image cap">
@@ -28,8 +30,8 @@ const menu = ` <div class="row m-5 p-5">
           <p class="card-text"> 15$</p>
         </div>
     </div>
-</div>
-<div class="col-4">
+ </div>
+ <div class="col-4">
 
     <div class="card mb-3" style="width: 18rem;">
         <img class="card-img-top" src="https://cdn.pixabay.com/photo/2015/03/26/09/39/cupcakes-690040_960_720.jpg" alt="Card image cap">
@@ -38,8 +40,8 @@ const menu = ` <div class="row m-5 p-5">
           <p class="card-text"> 10$</p>
         </div>
     </div>
-</div>
-<div class="col-4">
+ </div>
+ <div class="col-4">
 
     <div class="card mb-3" style="width: 18rem;">
         <img class="card-img-top" src="https://www.glutenfreepalate.com/wp-content/uploads/2018/08/Gluten-Free-Pizza-3.2-480x360.jpg" alt="Card image cap">
@@ -48,7 +50,9 @@ const menu = ` <div class="row m-5 p-5">
           <p class="card-text"> 12$</p>
         </div>
     </div>
-</div>
-</div>`;
+ </div>
+ </div>`;
+  return menuSection;
+};
 
 export default menu;
